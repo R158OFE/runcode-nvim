a9cd2238df0a6542f2e04f4b2aaac95801c24864
R158OFE <nhatoang063@gmail.com>
2026-06-19 19:03:00 +0700

    update key add run C# and exe

--- keymaps.lua
+++ keymaps.lua
@@ -1,7 +1,6 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
-
 vim.keymap.set("n", "<F5>", function()
   local ft = vim.bo.filetype
   local file = vim.fn.expand("%")
@@ -20,33 +19,32 @@ vim.keymap.set("n", "<F5>", function()
     end
   end
 
-  -- 3. Điều hướng cửa sổ: Nếu có rồi thì nhảy vào, chưa có thì split
+  -- 3. Điều hướng cửa sổ
   if term_win then
     vim.api.nvim_set_current_win(term_win)
   else
-    vim.cmd("split") -- Chỉ chẻ đôi màn hình nếu chưa có terminal nào mở
+    vim.cmd("split")
   end
 
-  -- 4. Kiểm tra ngôn ngữ và chạy lệnh tương ứng với Windows
+  -- 4. Xử lý logic chạy file
   if ft == "python" then
     vim.cmd("term python " .. file)
-    vim.cmd("startinsert")
-
   elseif ft == "javascript" then
     vim.cmd("term node " .. file)
-    vim.cmd("startinsert")
-
   elseif ft == "cpp" then
-    -- Cấu hình chuẩn cho Windows: dùng .exe và chạy bằng tên file
     vim.cmd("term g++ " .. file .. " -o " .. name .. ".exe && " .. name .. ".exe")
-    vim.cmd("startinsert")
-
   elseif ft == "c" then
-    -- Cấu hình chuẩn cho Windows: dùng .exe và chạy bằng tên file
     vim.cmd("term gcc " .. file .. " -o " .. name .. ".exe && " .. name .. ".exe")
-    vim.cmd("startinsert")
-
+  elseif ft == "cs" then
+    -- Chạy bằng dotnet cho dự án C#
+    vim.cmd("term dotnet run")
+  elseif ft == "dosini" or file:match("%.exe$") then
+    -- Nếu là file .exe thì gọi trực tiếp
+    vim.cmd("term " .. file)
   else
     print("Unsupported filetype: " .. ft)
+    return -- Dừng lại nếu không hỗ trợ
   end
+
+  vim.cmd("startinsert")
 end, { desc = "Run current file intelligently" })
